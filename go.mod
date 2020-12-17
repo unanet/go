c25c62fe747796405b7679a20365ca99afee9015
Casey Entzi <casey.entzi@silverblueprints.com>
2020-12-17 08:43:12 -0600

    adding k8s

--- go.mod
+++ go.mod
@@ -6,9 +6,15 @@ require (
 	github.com/go-chi/chi v4.1.0+incompatible
 	github.com/go-chi/render v1.0.1
 	github.com/go-ozzo/ozzo-validation/v4 v4.3.0
+	github.com/googleapis/gnostic v0.5.3 // indirect
+	github.com/imdario/mergo v0.3.11 // indirect
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus/client_golang v1.9.0
 	github.com/satori/go.uuid v1.2.0
 	go.uber.org/zap v1.16.0
+	k8s.io/api v0.20.0 // indirect
+	k8s.io/client-go v11.0.0+incompatible
+	k8s.io/klog v1.0.0 // indirect
+	k8s.io/utils v0.0.0-20201110183641-67b214c5f920 // indirect
 )
---
+++ pkg/k8s/k8s.go
@@ -0,0 +1,51 @@
+package k8s
+
+import (
+	"os"
+	"path/filepath"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+
+	"gitlab.unanet.io/devops/go/pkg/errors"
+)
+
+func GetInClusterK8sClient() (*kubernetes.Clientset, error) {
+	c, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, errors.Wrap(err)
+	}
+
+	client, err := kubernetes.NewForConfig(c)
+	if err != nil {
+		return nil, errors.Wrap(err)
+	}
+
+	return client, nil
+}
+
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+
+	return os.Getenv("USERPROFILE") // windows
+}
+
+func GetLocalConfigK8sClient() (*kubernetes.Clientset, error) {
+	configFile := filepath.Join(homeDir(), ".kube", "config")
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", configFile)
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+
+}
